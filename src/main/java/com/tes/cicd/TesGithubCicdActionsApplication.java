24db56ab6af9089ae50350460cbb4f8c1106aa3a
coop-infinity-developers <developer@coopbankoromia.com.et>
2026-01-02 15:39:31 +0300

    code

---
+++ src/main/java/com/tes/cicd/TesGithubCicdActionsApplication.java
@@ -0,0 +1,28 @@
+package com.tes.cicd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class TesGithubCicdActionsApplication {
+    @GetMapping("/welcome")
+    public String Welcome(){
+        return "Welcome to test git repository";
+    }
+    public static void main(String[] args) {
+        SpringApplication.run(TesGithubCicdActionsApplication.class, args);
+    }
+
+}
+
+
+//echo "# tes-github-cicd" >> README.md
+//git init
+//git add README.md
+//git commit -m "first commit"
+//git branch -M main
+//git remote add origin https://github.com/TESFAHUNS/tes-github-cicd.git
+//git push -u origin main
---
+++ src/test/java/com/tes/cicd/TesGithubCicdActionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tes.cicd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TesGithubCicdActionsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
